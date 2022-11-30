fdffa27b21ed4af48114fb44c9fc59019dd82086
jubayer <jubayertalha194@gmail.com>
2022-12-01 00:41:00 +0600

    add apache ignite docker

---
+++ demoapp/apache-ignite-docker/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+AKS_RG=aks-rg
+AKS_NAME=aks-poc
+
+ACR_NAME=myacr221
+ImageName=ignite
+ImageTag=v1.0
+Image=$ACR_NAME".azurecr.io/"$ImageName":"$ImageTag
+
+SourceImageName="apacheignite/ignite"
+SourceImageTag="latest"
+SourceImage=$SourceImageName":"$SourceImageTag
+
+az login --identity --username /subscriptions/863fda5d-6174-4448-8f7b-f8b0f9008431/resourcegroups/identity-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/myACRId
+az aks get-credentials --resource-group $AKS_RG --name $AKS_NAME
+az acr login --name $ACR_NAME
+
+docker pull $SourceImage
+docker tag $SourceImage $Image
+docker push $Image
+
+az acr repository list -n $ACR_NAME --output table
+
+helm install $ImageName ./helm
