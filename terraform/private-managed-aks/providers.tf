19fb6d2ba7958ef8a85b4c653a7cda9af8a45908
jubayer <jubayertalha194@gmail.com>
2022-12-07 02:06:39 +0600

    add auth ids

--- terraform/private-managed-aks/providers.tf
+++ terraform/private-managed-aks/providers.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   features {}
+    use_msi         = true
+    subscription_id = "863fda5d-6174-4448-8f7b-f8b0f9008431"
+    client_id       = "e3859ee3-8420-4c4f-b7ab-491a4edc30bd"
+    tenant_id       = "12025f5d-3ae2-41ea-8b1c-7b41d3f025c4"
 }
 
 terraform {
