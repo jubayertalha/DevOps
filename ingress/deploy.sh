3bde424d40de1b0a1f7358d8dbdb83d45a12a098
jubayer <jubayertalha194@gmail.com>
2023-01-05 04:43:11 +0600

    add ingress controller

--- ingress/deploy.sh
+++ ingress/deploy.sh
@@ -2,4 +2,5 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --set controller.extraArgs.default-ssl-certificate="default/tls-secret" \
   -f internal-ingress.yaml
