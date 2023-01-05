658fa53d604c1c93b8154dec7d81d0a4f8d2ad95
jubayer <jubayertalha194@gmail.com>
2023-01-05 20:31:16 +0600

    add tls

--- ingress-controller/deploy.sh
+++ ingress-controller/deploy.sh
@@ -2,5 +2,5 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 helm install ingress-nginx ingress-nginx/ingress-nginx \
-  --set controller.extraArgs.default-ssl-certificate="default/tls-secret" \
+  --set controller.extraArgs.default-ssl-certificate="default/selfca-ingress-tls" \
   -f internal-ingress.yaml
