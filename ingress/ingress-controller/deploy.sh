4a157a1c8bc552e88b2a64b5ae6090278ed82b77
jubayer <jubayertalha194@gmail.com>
2023-01-07 04:43:57 +0600

    add ingress

--- ingress/ingress-controller/deploy.sh
+++ ingress/ingress-controller/deploy.sh
@@ -2,5 +2,7 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace spectrum \
+  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz \
   --set controller.extraArgs.default-ssl-certificate="default/selfca-ingress-tls" \
   -f internal-ingress.yaml
