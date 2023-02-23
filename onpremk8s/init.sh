0e5a0b82996340b8ca1d38eeaaab127732644d7f
jubayer <jubayertalha194@gmail.com>
2023-02-23 21:26:18 +0600

    add on prem k8s init

--- onpremk8s/init.sh
+++ onpremk8s/init.sh
@@ -15,3 +15,24 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum install docker-ce-20.10.12 docker-ce-cli-20.10.12 -y
 systemctl enable docker --now
 docker version
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64 enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubelet-1.23.4 kubeadm-1.23.4 kubectl-1.23.4
+systemctl enable kubelet --now
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm -f get_helm.sh
+ln -s /usr/local/bin/helm /usr/bin/helm
+
+grep "net.ipv4.ip_forward" /etc/sysctl.conf && sed -i -e "s/(net.ipv4.ip_forward\s*\=\s*)0/\11/g" /etc/sysctl.conf || echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+grep "net.bridge.bridge-nf-call-iptables" /etc/sysctl.conf && sed -i -e "s/(net.bridge.bridge-nf-call-iptables\s*\=\s*)0/\11/g" /etc/sysctl.conf || echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
