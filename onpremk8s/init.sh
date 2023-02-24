b5a5cf19cf7b74ea38e43bc4189ab2e4e3796c7f
jubayer <jubayertalha194@gmail.com>
2023-02-24 19:21:17 +0600

    add on prem k8s init

--- onpremk8s/init.sh
+++ onpremk8s/init.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo su
-
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
 systemctl stop firewalld && systemctl disable firewalld
@@ -18,7 +16,9 @@ docker version
 
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
-name=Kubernetes baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64 enabled=1
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
 gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
