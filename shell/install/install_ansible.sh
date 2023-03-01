c8c525579fa12cf6b3a8468f7c6e1f79e276c21d
jubayer <jubayertalha194@gmail.com>
2023-03-01 22:57:56 +0600

    add install ansible

--- shell/install/install_ansible.sh
+++ shell/install/install_ansible.sh
@@ -15,6 +15,10 @@ sudo apt install ansible
 sudo apt install python3-argcomplete
 sudo activate-global-python-argcomplete3
 
-ansible-config init --disabled > ansible.cfg
+#ansible-config init --disabled > ansible.cfg
+#sudo chmod 600 /home/azureuser/kmaster_key.pem 
+#cat ansible.cfg | grep private_key_file
+#private_key_file = /home/azureuser/kmaster_key.pem
+#sed -i 's/#private_key_file =/private_key_file = \/home\/azureuser\/kmaster_key.pem/g' ansible.cfg
 
 ansible --version
