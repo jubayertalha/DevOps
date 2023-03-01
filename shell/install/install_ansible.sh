846570472b0daf0c4bc995c1b26716c3e0aa4df0
jubayer <jubayertalha194@gmail.com>
2023-03-01 22:18:38 +0600

    add install ansible

---
+++ shell/install/install_ansible.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sudo apt remove ansible
+sudo apt --purge autoremove
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt update
+
+sudo apt install ansible
+
+sudo apt install python3-argcomplete
+sudo activate-global-python-argcomplete3
+
+ansible --version
